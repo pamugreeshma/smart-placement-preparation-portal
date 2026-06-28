f9ee4b69720420b4038d78dd791c3a848d3e03e0
Greeshma <pamugreeshma22@gmail.com>
2026-06-28 23:17:10 +0530

    Authentication routing completed

---
+++ app.py
@@ -0,0 +1,51 @@
+from routes.main import main
+
+from routes.auth import auth
+
+from flask import Flask, render_template
+
+from flask_login import LoginManager
+
+from config import Config
+
+from models import db
+
+from models.user import User
+
+
+login_manager = LoginManager()
+
+
+def create_app():
+
+    app = Flask(__name__)
+
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    login_manager.init_app(app)
+
+    login_manager.login_view = "auth.login"
+
+    with app.app_context():
+        db.create_all()
+
+    app.register_blueprint(main)
+    app.register_blueprint(auth)
+    
+    return app
+
+
+@login_manager.user_loader
+def load_user(user_id):
+
+    return User.query.get(int(user_id))
+
+
+app = create_app()
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
---
+++ static/js/main.js
@@ -0,0 +1,1 @@
+console.log("CareerForge Loaded Successfully 🚀");
