2e2ecf418462843093f778ce46e1c29ead609937
Greeshma <pamugreeshma22@gmail.com>
2026-08-01 00:36:01 +0530

    Fixed Render database path

--- config.py
+++ config.py
@@ -2,12 +2,15 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+INSTANCE_DIR = os.path.join(BASE_DIR, "instance")
+os.makedirs(INSTANCE_DIR, exist_ok=True)
+
 
 class Config:
     SECRET_KEY = "careerforge-secret-key"
 
     SQLALCHEMY_DATABASE_URI = (
-        "sqlite:///" + os.path.join(BASE_DIR, "instance", "careerforge.db")
+        "sqlite:///" + os.path.join(INSTANCE_DIR, "careerforge.db")
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
